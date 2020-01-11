80b633deb105d08ab7b2eeb476c2fbcaa814b14e
Parul Veda <parulveda1@gmail.com>
2020-01-11 13:56:11 -0800

    Finished program

--- 00_rainbow.py
+++ 00_rainbow.py
@@ -1,24 +1,13 @@
-def write_colorful_text():    #(string, style, foreground, background): 
-    for style in range(8):
-        for foreground_color in range(30,38): 
-            s1 = ''
-            for background_color in range(40,48):
-                format = ';'.join([str(style), str(foreground_color), str(background_color)]) 
-                s1 += '\x1b[%sm %s \x1b[0m' % (format, x)
-            print(s1) 
-        print('\n')
+def write_colorful_text(string, style, foreground, background):
+    s1 = ""
+    format = ';'.join([str(style), str(foreground), str(background)])
+    s1+= '\x1b[%sm %s \x1b[0m' % (format, string)
+    print(s1)
 
-x = ['R','A', 'I', 'N', 'B', 'O', 'W']
-
-#write_colorful_text("R", \33[1m, \33[31m, \33[40m)
-write_colorful_text()
-
-def write_color_text(string, style, foreground, background):
-    for style in range(8):
-        for foreground in range(30, 38):
-            s1 = ""
-            for background in range(40, 48):
-                format = ';'.join([str(style), str(foreground), str(background)])
-                s1 += '\x1b[%sm %s \x1b[0m' % (format, R)
-            print(s1)
-        print('\n')
+write_colorful_text("R", 8, 31, 40)
+write_colorful_text("A", 8, 32, 40)
+write_colorful_text("I", 8, 33, 40)
+write_colorful_text("N", 8, 36, 40)
+write_colorful_text("B", 8, 34, 40)
+write_colorful_text("O", 8, 35, 40)
+write_colorful_text("W", 8, 38, 40)
